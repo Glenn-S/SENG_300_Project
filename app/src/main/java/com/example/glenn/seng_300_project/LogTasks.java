0685c63c66ca567728666c9374b20a0c5c63b980
Glenn-S <34552455+Glenn-S@users.noreply.github.com>
2018-06-10 06:44:32 -0600

    commit to pull

---
+++ app/src/main/java/com/example/glenn/seng_300_project/LogTasks.java
@@ -0,0 +1,22 @@
+package com.example.glenn.seng_300_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LogTasks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_tasks);
+
+
+    }
+
+    public void setTaskColour(){
+
+        return;
+    }
+
+
+}
--- app/src/main/java/com/example/glenn/seng_300_project/MainActivity.java
+++ app/src/main/java/com/example/glenn/seng_300_project/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.glenn.seng_300_project;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // simpl
+        Button button = (Button) findViewById(R.id.btnLogTasks);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), LogTasks.class);
+                startActivity(startIntent);
+            }
+        });
     }
+
 }
