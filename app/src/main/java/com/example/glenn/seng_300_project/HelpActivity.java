1d58fc4746c72f390d2c545015f2c940653b43b0
anthony-t <abtran@ucalgary.ca>
2018-06-10 22:35:03 -0600

    Added activity for help screen, navigation drawer, and task management screen.

---
+++ app/src/main/java/com/example/glenn/seng_300_project/HelpActivity.java
@@ -0,0 +1,14 @@
+package com.example.glenn.seng_300_project;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class HelpActivity extends NavigationBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_help);
+    }
+
+}
--- app/src/main/java/com/example/glenn/seng_300_project/MainActivity.java
+++ app/src/main/java/com/example/glenn/seng_300_project/MainActivity.java
@@ -6,12 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends NavigationBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.setContentView(R.layout.activity_main);
 
         // simpl
         Button button = (Button) findViewById(R.id.btnLogTasks);
