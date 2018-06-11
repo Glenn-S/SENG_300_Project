e279a2a06a712ba701d7152ac7bb9bf5712d998f
Glenn-S <34552455+Glenn-S@users.noreply.github.com>
2018-06-11 15:23:16 -0600

    took out commit handles

--- app/src/main/java/com/example/glenn/seng_300_project/Pop.java
+++ app/src/main/java/com/example/glenn/seng_300_project/Pop.java
@@ -4,59 +4,42 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-<<<<<<< HEAD
 import android.support.design.widget.FloatingActionButton;
-=======
->>>>>>> joseph
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-<<<<<<< HEAD
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.w3c.dom.Text;
-=======
 import android.widget.ListView;
->>>>>>> joseph
-
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-<<<<<<< HEAD
 import java.util.List;
-=======
->>>>>>> joseph
+
 
 public class Pop extends Activity {
 
     ListView listView;
     ArrayAdapter<String> adapter;
-<<<<<<< HEAD
+
     // set up the add task button
     FloatingActionButton addBtn;
-
-=======
->>>>>>> joseph
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
+
         //set the window properties
-=======
->>>>>>> joseph
         setContentView(R.layout.popwindow);
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         int width = dm.widthPixels;
         int height = dm.heightPixels;
         getWindow().setLayout((int)(width*0.7), (int)(height*0.5));
-
-<<<<<<< HEAD
 
         // populate the list view
         TaskManager mTasks = new TaskManager(Pop.this);
@@ -71,7 +54,7 @@ public class Pop extends Activity {
 
         // set button and list properties
         addBtn = (FloatingActionButton) findViewById(R.id.fabAddTask);
-=======
+
         // populate the list view
         ArrayList<String> customTaskList = new ArrayList<String>();
         customTaskList.addAll(Arrays.asList("Coding",
@@ -81,12 +64,11 @@ public class Pop extends Activity {
                                             "Worked on the big project for Encana that is due at the end of the quarter",
                                             "Ran Errands"));
 
->>>>>>> joseph
+
         listView = (ListView)findViewById(R.id.customTasksList);
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, customTaskList);
         listView.setAdapter(adapter);
 
-<<<<<<< HEAD
         // get value of extra passed from previous activity, this contains the value being passed
         final String tasksTime = getIntent().getStringExtra("time");
         TextView tv = (TextView) findViewById(R.id.textView3);
@@ -95,13 +77,11 @@ public class Pop extends Activity {
         customTaskList.addAll(Arrays.asList("Coding", "Meeting", "Coffee Break", "Checked Emails", "Worked on the big project for Encana that is due at the end of the quarter", "Ran Errands"));
 
         // for adding a new item to the log of tasks
-=======
->>>>>>> joseph
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-<<<<<<< HEAD
+
                 //setFile(customTaskList.get(position)); // this order may not work
                 finish();
             }
@@ -117,11 +97,9 @@ public class Pop extends Activity {
                 listView.invalidateViews();
                 Toast.makeText(Pop.this, "Added Task", Toast.LENGTH_SHORT).show();
                 //finish();
-            }
-=======
                 finish();
             }
->>>>>>> joseph
+
         });
     }
 }
